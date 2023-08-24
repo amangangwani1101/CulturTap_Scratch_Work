959c008642327ff27b46da23cbf7d19bfcf88d59
Utkarsh <Aishwary.009.shrivastava@culturtap.com>
2023-08-24 11:36:07 +0530

    Phone Number page updated which is named as secondPage invalid number case added in the code

--- lib/FirstPage.dart
+++ lib/FirstPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/SecondPage.dart';
 
-
 void main() {
   runApp(const FirstPage());
 }
@@ -12,15 +11,15 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Center(
-          child: const MyHomePage(title: 'LogIn'),
-        ),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Center(
+        child: const MyHomePage(title: 'LogIn'),
+      ),
     );
   }
 }
@@ -38,112 +37,112 @@ class _MyHomePageState extends State<MyHomePage> {
   var nameText = TextEditingController();
   bool validate = false;
 
-  // void _navigateToNextPage() {
-  //   if (nameText.text.isNotEmpty) {
-  //     Navigator.push(
-  //       context,
-  //       MaterialPageRoute(
-  //         builder: (context) => NextPage(name: nameText.text),
-  //       ),
-  //     );
-  //   }
-  // }
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Center(
-            child: Text(
-              'CULTURTAP',
-              style: TextStyle(
-                color: Colors.orange,
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            'CULTURTAP',
+            style: TextStyle(
+              color: Colors.orange,
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
             ),
-          )),
+          ),
+        ),
+      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(30),
           child: Container(
-              width: double.infinity,
-              child: Center(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                          margin: const EdgeInsets.only(bottom: 35),
-                          height: 300,
-                          color: Colors.white54),
-                      Text('FirstPage',
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold)),
-                      Container(
-                          margin: EdgeInsets.only(bottom: 31),
-                          child: Text('Explore, Update, Guide & Earn !',
-                              style: TextStyle(
-                                  fontSize: 25, color: Colors.black))),
-                      Text('Please Enter Your Name',
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold)),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 19),
-                        width: 300,
-                        child: TextField(
-                          controller: nameText,
-                          decoration: InputDecoration(
-                            hintText: 'Ex : Kishor Kumar',
-                          ),
+            width: double.infinity,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 35),
+                      height: 300,
+                      color: Colors.white54,
+                    ),
+                    Text('FirstPage',
+                        style: TextStyle(
+                            fontSize: 35,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold)),
+                    Container(
+                        margin: EdgeInsets.only(bottom: 31),
+                        child: Text('Explore, Update, Guide & Earn !',
+                            style: TextStyle(fontSize: 25, color: Colors.black))),
+                    Text('Please Enter Your Name',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold)),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 19),
+                      width: 300,
+                      child: TextField(
+                        controller: nameText,
+                        decoration: InputDecoration(
+                          hintText: 'Ex : Kishor Kumar',
                         ),
                       ),
-                      Container(
-                          margin: EdgeInsets.only(bottom: 21),
-                          child: Row(
-                            children: [
-                              Text('Already User ?',
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w100)),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text('Sign In',
-                                    style: TextStyle(
-                                      color: Colors.orange,
-                                      fontWeight: FontWeight.w900,
-                                      fontSize: 25,
-                                    )),
-                              )
-                            ],
-                          )),
-                      Container(
-                          width: 330,
-                          height: 70,
-                          child: FilledButton(
-                              backgroundColor:
-                              Colors.orange,
-                              onPressed: () {
-                                String Name = nameText.text.toString();
-                                print('Name  : ${Name} ');
-                                Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage(),));
-                              },
-                              child: Center(
-                                  child: Text('Next',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                          fontSize: 25))))
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 21),
+                      child: Row(
+                        children: [
+                          Text('Already User ?',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w100)),
+                          TextButton(
+                            onPressed: () {
+                              // TODO: Add navigation to Sign In page
+                            },
+                            child: Text('Sign In',
+                                style: TextStyle(
+                                  color: Colors.orange,
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 25,
+                                )),
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    Container(
+                      width: 330,
+                      height: 70,
+                      child: FilledButton(
+                          backgroundColor: Colors.orange,
+                          onPressed: () {
+                            String Name = nameText.text.toString();
+                            print('Name  : ${Name} ');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SecondPage(
+                                  phoneNumberController: TextEditingController(),
+                                ),
+                              ),
+                            );
+                          },
+                          child: Center(
+                              child: Text('Next',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 25)))),
+                    ),
+                  ],
                 ),
-              )),
+              ),
+            ),
+          ),
         ),
       ),
     );
@@ -173,33 +172,3 @@ class FilledButton extends StatelessWidget {
     );
   }
 }
-//
-// class NextPage extends StatelessWidget {
-//   final String name;
-//
-//   const NextPage({Key? key, required this.name}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Next Page'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text('Welcome, $name!', style: TextStyle(fontSize: 20)),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {
-//                 Navigator.pop(context);
-//               },
-//               child: Text('Go Back'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/SecondPage.dart
+++ lib/SecondPage.dart
@@ -1,88 +1,152 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/ThirdPage.dart';
 
-
-class SecondPage extends StatelessWidget {
-  var PhoneNumber = TextEditingController();
+class PhoneNumberValidator extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        appBar : AppBar(
-          title : Text('INTRO'),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(30),
-            child: Container(
-                width: double.infinity,
-                child: Center(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                            margin: const EdgeInsets.only(bottom: 35),
-                            height: 300,
-                            color: Colors.white54),
-                        Text('SIGNUP',
-                            style: TextStyle(
-                                fontSize: 35,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold)),
-                        Container(
-                            margin: EdgeInsets.only(bottom: 31),
-                            child: Text('Start Your Adventure now !',
-                                style: TextStyle(
-                                    fontSize: 25, color: Colors.black))),
-                        Text('Please Enter Your Number',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold)),
-                        Container(
-                          margin: EdgeInsets.only(bottom: 19),
-                          width: 300,
-                          child:
-                          TextField(
-                            controller: PhoneNumber,
+  _PhoneNumberValidatorState createState() => _PhoneNumberValidatorState();
+}
 
-                            decoration: InputDecoration(
-                              hintText: '+91 '  + 'Ex : 9026966203',
+class _PhoneNumberValidatorState extends State<PhoneNumberValidator> {
+  final TextEditingController _phoneNumberController = TextEditingController();
+
+  @override
+  void dispose() {
+    _phoneNumberController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SecondPage(
+      phoneNumberController: _phoneNumberController,
+    );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  final TextEditingController phoneNumberController;
+
+  SecondPage({
+    required this.phoneNumberController,
+  });
+
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  bool _isPhoneNumberValid = true; // Default to true
+
+  // Function to validate phone number using regex
+  bool validatePhoneNumber(String input) {
+    final RegExp regex = RegExp(r'^\d{10}$');
+    return regex.hasMatch(input);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('INTRO'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Container(
+            width: double.infinity,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 35),
+                      height: 300,
+                      color: Colors.white54,
+                    ),
+                    Text(
+                      'SIGNUP',
+                      style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 31),
+                      child: Text(
+                        'Start Your Adventure now !',
+                        style: TextStyle(fontSize: 25, color: Colors.black),
+                      ),
+                    ),
+                    Text(
+                      'Please Enter Your Number',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 19),
+                      width: 300,
+                      child: TextField(
+                        controller: widget.phoneNumberController,
+                        keyboardType: TextInputType.phone,
+                        onChanged: (value) {
+                          setState(() {
+                            _isPhoneNumberValid = true; // Reset to true on change
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: '+91 ' + 'Ex : 9026966203',
+                          errorText: _isPhoneNumberValid
+                              ? null
+                              : 'Invalid Phone Number',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 330,
+                      height: 70,
+                      child: FilledButton(
+                        backgroundColor: Colors.orange,
+                        onPressed: () {
+                          String number = widget.phoneNumberController.text;
+                          bool isValid = validatePhoneNumber(number);
+
+                          setState(() {
+                            _isPhoneNumberValid = isValid;
+                          });
+
+                          if (isValid) {
+                            print('PhoneNumber: $number');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ThirdPage()),
+                            );
+                          }
+                        },
+                        child: Center(
+                          child: Text(
+                            'Next',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 25,
                             ),
                           ),
                         ),
-
-
-
-                        Container(
-                            width: 330,
-                            height: 70,
-                            child: FilledButton(
-                              backgroundColor: Colors.orange,
-
-                                onPressed: () {
-                                  String Number = PhoneNumber.text;
-                                  print('PhoneNumber  : ${Number} ');
-                                  Navigator.push(context, MaterialPageRoute(builder: (context) => ThirdPage(),));
-                                },
-                                child: Center(
-                                    child: Text('Next',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white,
-                                            fontSize: 25))))
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                )),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
-
+      ),
     );
   }
-
 }
 
 class FilledButton extends StatelessWidget {
