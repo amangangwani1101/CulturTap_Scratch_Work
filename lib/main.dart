bd20c0d6ed6da01b9299e50a110e4357ed4c647a
Utkarsh <Aishwary.009.shrivastava@culturtap.com>
2023-08-26 02:42:05 +0530

    changed lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/FirstPage.dart';
-import 'package:learn_flutter/IntroPage.dart';
 
 
 Future<void> main() async{
