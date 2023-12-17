951fd5f4bb07cfde07cb610e7b99b47aa2edc151
Aishwary <Aishwary.009.shrivastava@culturtap.com>
2023-12-17 06:04:47 +0530

    codes working

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 //homepage
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/CulturTap/VideoFunc/categoryData.dart';
 import 'package:learn_flutter/CulturTap/VideoFunc/category_section_builder.dart';
@@ -19,7 +20,10 @@ import 'package:flutter/rendering.dart';
 import 'UserProfile/ProfileHeader.dart';
 
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
