a4179ca0eea2c569cd3fbf4c47399dcbe0a58886
Aishwary <Aishwary.009.shrivastava@culturtap.com>
2024-02-15 12:28:37 +0530

    push

--- server/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://utkarsh:utkarshG1234@culturtap.t5vwtzg.mongodb.net/userStoryDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-}).then(()=>{
-    console.log('mongodb connected');
-}).catch((error)=>{
-console.log(error)
-});
